2738c81badd460cbd22a8141ba2f08f2f7e10f42
OliBN <olibn333@gmail.com>
2019-01-01 17:08:57 +0630

    added htmlparser2 demo test

--- redditScrape.js
+++ redditScrape.js
@@ -1,9 +1,42 @@
 const cheerio = require('cheerio');
 const request = require('request')
+const htmlparser = require('htmlparser2')
 
 let resultData = []
 
-request('https://www.reddit.com/r/futurology', function (error, response, body) {
+//TEST RUN:
+//htmlparser1()
+cheerio1()
+
+
+//htmlparser2 test - needs custom functions to traverse the resulting DOM
+function htmlparser1(){
+  request('https://www.reddit.com/r/futurology', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+
+    const dom = htmlparser.parseDOM(body)
+    const articles = htmlparser.DomUtils.find((a)=>{return (a.name=='article')},dom,1)
+
+    articles.forEach(function(element, i) {
+      const miniDOM = []
+      miniDOM.push(element)
+
+    //first argument of DomUtils.find is a function that tests if result is true. Might as well write your own? Or better yet write a CSS selector for Domutils...
+    const links = htmlparser.DomUtils.find((a)=>{return (a.name=='a')},miniDOM,1)
+    const heads = htmlparser.DomUtils.find((a)=>{return (a.name=='h2')},links,1)
+    const title = htmlparser.DomUtils.find((a)=>{return (a.type=='text')},heads,1)[0].data
+    const articleDetails = {i, title}
+    resultData.push(articleDetails)
+    
+  })
+  console.log(resultData)
+  }});
+};
+
+
+//cheerio test - nice CSS selectors make things easier...
+function cheerio1 (){
+  request('https://www.reddit.com/r/futurology', function (error, response, body) {
   if (!error && response.statusCode == 200) {
   
   const baseUrl = 'https://www.reddit.com'
@@ -20,6 +53,8 @@ request('https://www.reddit.com/r/futurology', function (error, response, body) 
     resultData.push(articleDetails)
   })
   console.log(resultData)
+  
   }
 
-});
+})
+};
