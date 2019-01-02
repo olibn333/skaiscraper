e8eeb66a79462432619bc49ce8c7a0619e24f9db
OliBN <olibn333@gmail.com>
2019-01-02 20:09:44 +0630

    uname and password callbacks for mongodb

--- mongoStore.js
+++ mongoStore.js
@@ -1,34 +1,44 @@
 const readline = require('readline');
 
+storeInit('a')
 
-function storeInit(){
-  waitforPassword()
+function storeInit(file) {
+  sendToDB(file)
 }
 
-function handleAnswer(answer) {
-  console.log("Your password is " + answer + "?")
+function sendToDB(file) {
+  let uname, pword
+  promptUserNameandPassword(printResult)
 }
 
-function waitforPassword() {
+function printResult(a,b) {
+  console.log("Got " + a + " and " + b)
+}
+
+function promptUserNameandPassword(callback) {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  rl.question('MongoDB Password:', (answer) => {
-    handleAnswer(answer)
+  rl.question('MongoDB Username:', (answer) => {
+    rl.question('MongoDB Password:', (answer2) => {
+      callback(answer,answer2)
     rl.close();
+    })
+    
   });
 };
 
+function sendToMongoDB(username, password, file) {
+  const MongoClient = require('mongodb').MongoClient;
+  const url = "mongodb+srv://" + username + ":" + password + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
 
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0-ywxua.mongodb.net/test?retryWrites=true";
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database Connected!");
+    db.close();
+  });
+}
 
-// MongoClient.connect(url, function (err, db) {
-//   if (err) throw err;
-//   console.log("Database Connected!");
-//   db.close();
-// });
-
-module.exports = {storeInit}
+module.exports = { storeInit }
