1492eb6f6653bf7d2da1ed77991aaca3372bc414
joss-c <joss.classey@gmail.com>
2019-01-04 02:26:37 +0400

    refactor(redditScrape): Improved timestamp insertion and object creation for reusability

--- redditScrape.js
+++ redditScrape.js
@@ -2,9 +2,6 @@ const cheerio = require('cheerio');
 const request = require('request')
 const mongoStore = require('./mongoStore')
 
-//Create timestamp to create object + make scrape accessible
-const timestampNow = new Date().toISOString()
-
 scrapeInit()
 
 //Error log as object
@@ -27,27 +24,26 @@ function scrapeInit() {
   console.log('Scraping...')
 }
 
-function scrapeResultHandler(result, errs) {
-  console.log(Object.keys(result.reddit.scrapes[timestampNow].articles).length + " Articles scraped.")
+function scrapeResultHandler(result, site, errs) {
+  const scrapes = result[site].scrapes
+  //Array of timestamps
+  const timestamps = Object.keys(scrapes)
+  //Use pop() to get last timestamp key from array
+  console.log(Object.keys(scrapes[timestamps.pop()]).length + " Articles scraped.")
   console.log(errs + " details not found.")
   console.log(result)
-  console.log(result.reddit.scrapes)
+  console.log(result[site].scrapes)
 
   mongoStore.storeInit(result)
 }
 
 //Build objects for scrape result data
-function createResultsObject(name, baseUrl, timestamp) {
+function createResultsObject(name, baseUrl) {
   return {
     [name]: {
       'baseUrl': baseUrl,
       scrapes: {
-        [timestamp]: {
-          articles: {
-
-          }
         }
-      }
     }
   }
 }
@@ -58,8 +54,14 @@ function getRedditArticles(url) {
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-      //Create object to contain articles (db.site.scrapes.timestamp.articles)
-      let resultData = createResultsObject('reddit', 'https://www.reddit.com', timestampNow)
+      //Create object to contain articles (db.site.scrapes.timestamp)
+      let resultData = createResultsObject('reddit', 'https://www.reddit.com')
+
+      //Create timestamp for scrape session
+      const timestampNow = new Date().toISOString()
+
+      //Create empty object with timestamp
+      resultData.reddit.scrapes[timestampNow] = {}
 
       //Scrape details with cheerio
       const $ = cheerio.load(body)
@@ -89,13 +91,14 @@ function getRedditArticles(url) {
         //Article URL
         articleUrl = checkUndefined( $('div a', element).eq(4).attr('href') )
 
-        //
-        const articleDetails = { i, titleText, commentsUrl, picUrl, articleUrl }        
-        resultData.reddit.scrapes[timestampNow].articles[i] = articleDetails
-        
+        //Fill timestamped object with scrape results
+        i += 1
+        const articleDetails = { i, titleText, commentsUrl, picUrl, articleUrl }
+        let currentScrape = resultData.reddit.scrapes[timestampNow]
+        currentScrape[i] = Object.assign({}, articleDetails)
       })
       //callback
-      scrapeResultHandler(resultData, errorLog.errorCount)
+      scrapeResultHandler(resultData, 'reddit', errorLog.errorCount)
     }
   })
 }
