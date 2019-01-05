55a1813dd51abee01866cd071500b0bf62845424
olibn <olibn333@gmail.com>
2019-01-05 12:49:59 +0630

    document structure
    flattened document structure to store in DB for easier filters

--- mongoStore.js
+++ mongoStore.js
@@ -1,18 +1,12 @@
 const readline = require('readline');
+const MongoClient = require('mongodb').MongoClient;
 
-//storeInit('a')
-
-function storeInit(file) {
-  sendToDB(file)
-}
 
 function sendToDB(file) {
   promptUserNameandPassword(file, handleResult)
 }
 
 function handleResult(uname,pword,file) {
-  // console.log("Got " + a + " and " + b)
-  // console.log("Data: " + file.length + " articles")
   sendToMongoDB(uname,pword,file)
 }
 
@@ -24,7 +18,7 @@ function promptUserNameandPassword(file, callback) {
 
   rl.question('MongoDB Username: ', (uname) => {
     rl.question('MongoDB Password: ', (pword) => {
-      callback(uname,pword, file)
+      callback(uname, pword, file)
     rl.close();
     })
     
@@ -32,20 +26,19 @@ function promptUserNameandPassword(file, callback) {
 };
 
 function sendToMongoDB(username, password, file) {
-  const MongoClient = require('mongodb').MongoClient;
   const url = "mongodb+srv://" + username + ":" + password + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
 
   MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     if (err) throw err;
-    console.log("Database Connected! Ready to send " + file.length + " articles");
+    console.log("Database Connected! Sending " + file.articlesArray.length + " articles");
     const dbo = db.db('skaiScraper')
     const scraperCollection = dbo.collection('sites')
     scraperCollection.insertOne(file, function(err, res) {
       if (err) throw err;
-      console.log("Inserted " + res.ops.length + " articles.")
+      console.log("Inserted " + res.ops.length + " document(s).")
     })
     db.close();
   });
 }
 
-module.exports = { storeInit }
+module.exports = { sendToDB }
--- redditScrape.js
+++ redditScrape.js
@@ -4,65 +4,42 @@ const mongoStore = require('./mongoStore')
 
 scrapeInit()
 
+
 //Error log as object
 let errorLog = {
-    errorCount: 0
+  errorCount: 0,
+  errorsArray: []
 }
+
 
 //Checks if query returns undefined
 function checkUndefined(query) {
-    if (query === undefined) {
-        errorLog.errorCount += 1
-        return "Not found"
-    } else {
-        return query
-    }
+  if (query === undefined) {
+    errorLog.errorCount += 1
+    return "Not found"
+  } else {
+    return query
+  }
 }
 
 function scrapeInit() {
   const url = 'https://www.reddit.com/r/futurology'
   getRedditArticlesFromSubreddit(url)
   console.log('Scraping ' + url + '...')
- }
-
-
-function scrapeResultHandler(url, resultData, errs) {
-  // const scrapes = result[site].scrapes
-  //Array of timestamps
-  // const timestamps = Object.keys(scrapes)
-  //Use pop() to get last timestamp key from array
-  //console.log(Object.keys(scrapes[timestamps.pop()]).length + " Articles scraped.")
-  //console.log(result)
-  //console.log(timestamps)
-  //console.log(result[site].scrapes)
-
-  // console.log(result.site.subCategory.scrape.articles.length + " Article(s) Scraped.")
-  // console.log(errs + " Detail(s) not found.")
-  // console.log("Test title call: ", result.site.subCategory.scrape.articles[23].titleText)
-  // console.log(result)
-
-  mongoStore.storeInit(createSiteResultsObject(url,resultData))
 }
 
-//Build objects for scrape result data
-function createResultsObject(name, baseUrl) {
-  return {
-    [name]: {
-      'baseUrl': baseUrl,
-      scrapes: {
-        }
-    }
-  }
-}
 
-function createSiteResultsObject(url, articles) {
+function scrapeResultHandler(scrapeResultsObject) {
+
+  console.log(scrapeResultsObject.articlesArray.length + " Articles scraped.")
+  console.log(scrapeResultsObject.errorLog.errorCount + " Details not found.")
   
-  //TODO - Make generic reddit scraper to create the correct object result for any subreddit url. 
-  //Could pull [name] and [baseUrl] from the url argument of this function. May need to add subreddit object in our structure?
-  // Perhaps could also make a url parsing module for this?
+  mongoStore.sendToDB(scrapeResultsObject)
+}
 
-  const timestampNow = new Date()
+function createScrapeResultsObject(url, errorLog, articlesArray) {
 
+  const scrapeTimestamp = new Date()
   const domainName = extractRootDomain(url)
   const hostName = extractHostname(url)
   const siteName = domainName.split('.')[0]
@@ -71,47 +48,16 @@ function createSiteResultsObject(url, articles) {
   const subReddit = url.split('/r/')[1]
   const rootSite = url.split('/r/')[0]
 
-  //console.log("Some potential variable names:", rootSite, domainName, subReddit, hostName, siteName)
-
-  //trying to imagine the top level object for easy indexing. 
-  //constant key strings seems better performance for indexing the mongodb
-  //   return {
-  //     'site' : {
-  //       'siteName' : siteName,
-  //       'siteUrl' : rootSite,
-  //       'subCategory' : {
-  //         'name' : subReddit,
-  //         'url' : url,
-  //         'scrape' : {
-  //           'timestamp' : timestampNow,
-  //           'articles' : [
-  //             //articleDetails here
-  //           ]
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-
-return {siteName, rootSite, subReddit, url, timestampNow, articles}
+  return { url, siteName, rootSite, subReddit, scrapeTimestamp, errorLog, articlesArray }
 }
 
 function getRedditArticlesFromSubreddit(url) {
-  
+
   //Connect
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
 
-      let resultData = []
-      //let resultData = createSiteResultsObject(url)
-      //Create object to contain articles (db.site.scrapes.timestamp)
-      //let resultData = createResultsObject('reddit', 'https://www.reddit.com')
-
-      //Create timestamp for scrape session
-      //const timestampNow = new Date().toISOString()
-
-      //Create empty object with timestamp
-      //resultData.reddit.scrapes[timestampNow] = {}
+      let articlesArray = []
 
       //Scrape details with cheerio
       const $ = cheerio.load(body)
@@ -123,56 +69,50 @@ function getRedditArticlesFromSubreddit(url) {
 
         //Title
         titleEl = $('a h2', element)
-        titleText = checkUndefined( titleEl.text() )
+        titleText = checkUndefined(titleEl.text())
 
         //Reddit URL
         //commentsUrl = resultData.site.url + checkUndefined( titleEl.parent().attr('href') )
-        commentsUrl = checkUndefined( titleEl.parent().attr('href') )
+        commentsUrl = checkUndefined(titleEl.parent().attr('href'))
 
         //Image URL
         try {
           const picEl = $('a div[role=img]', element)
           picUrl = picEl.css('background-image').slice(4, -1)
         }
-        catch (e) { 
-          picUrl = 'Not Found' 
+        catch (e) {
+          picUrl = "Not Found"
           errorLog.errorCount += 1
+          errorLog.errorsArray.push(e)
         }
 
         //Article URL
-        articleUrl = checkUndefined( $('div a', element).eq(4).attr('href') )
+        articleUrl = checkUndefined($('div a', element).eq(4).attr('href'))
 
-        //Fill timestamped object with scrape results
-        //i += 1
+        const articleDetails = { i, titleText, commentsUrl, picUrl, articleUrl }
 
-        const articleDetails = {i, titleText, commentsUrl, picUrl, articleUrl}
-        
-        //resultData.site.subCategory.scrapes.articles.push(articleDetails)
-        //resultData.site.subCategory.scrape.articles.push(articleDetails)
-        resultData.push(articleDetails)
-
-        // let currentScrape = resultData.reddit.scrapes[timestampNow]
-        // currentScrape[i] = Object.assign({}, articleDetails)
+        articlesArray.push(articleDetails)
 
       })
+
       //callback
-      //const result = createSiteResultsObject(url, resultData)
-      scrapeResultHandler(url, resultData, errorLog.errorCount)
+      const scrapeResultsObject = createScrapeResultsObject(url, errorLog, articlesArray)
+      scrapeResultHandler(scrapeResultsObject)
     }
   })
 }
 
 
-//ripped ruthlessly from https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
+//Ripped ruthlessly from https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
 function extractHostname(url) {
   let hostname;
   //find & remove protocol (http, ftp, etc.) and get hostname
 
   if (url.indexOf("//") > -1) {
-      hostname = url.split('/')[2];
+    hostname = url.split('/')[2];
   }
   else {
-      hostname = url.split('/')[0];
+    hostname = url.split('/')[0];
   }
 
   //find & remove port number
@@ -186,18 +126,18 @@ function extractHostname(url) {
 // To address those who want the "root domain," use this function:
 function extractRootDomain(url) {
   let domain = extractHostname(url),
-      splitArr = domain.split('.'),
-      arrLen = splitArr.length;
+    splitArr = domain.split('.'),
+    arrLen = splitArr.length;
 
   //extracting the root domain here
   //if there is a subdomain 
   if (arrLen > 2) {
-      domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
-      //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
-      if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
-          //this is using a ccTLD
-          domain = splitArr[arrLen - 3] + '.' + domain;
-      }
+    domain = splitArr[arrLen - 2] + '.' + splitArr[arrLen - 1];
+    //check to see if it's using a Country Code Top Level Domain (ccTLD) (i.e. ".me.uk")
+    if (splitArr[arrLen - 2].length == 2 && splitArr[arrLen - 1].length == 2) {
+      //this is using a ccTLD
+      domain = splitArr[arrLen - 3] + '.' + domain;
+    }
   }
   return domain;
 }
