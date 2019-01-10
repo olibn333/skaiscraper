d90db11bfc2f8486dd0c04518ded3a470fd8589c
joss-c <joss.classey@gmail.com>
2019-01-10 14:11:07 +0400

    feat(getMongoData): Styled table with material-ui

--- frontend/gatsby-site/src/pages/getMongoData.js
+++ frontend/gatsby-site/src/pages/getMongoData.js
@@ -1,28 +1,38 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
-export default ({ data }) => {
+const articles = ({ data }) => {
   console.log(data)
   return (
     <Layout>
-      <div>
-        <h1>Articles:</h1>
-        <table>
-          <thead>
-            <tr>
-              <th>Article Comments</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.allMongodbSkaiScraperreferencedArticles.edges.map(({ node }, index) => (
-              <tr key={index}>
-                <td><a href={node.commentsUrl}>{node.titleText}</a></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <Paper style={{ padding: '10px' }}>
+        <div>
+          <h1>Articles:</h1>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <th>Article Comments</th>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.allMongodbSkaiScraperreferencedArticles.edges.map(({ node }, index) => (
+                <TableRow key={index}>
+                  <TableCell>
+                    <a href={node.commentsUrl}>{node.titleText}</a>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </Paper>
     </Layout>
   )
 }
@@ -39,3 +49,5 @@ export const query = graphql`
     }
   }
 `
+
+export default articles
