84af8b83d3530226b33d950a8d62cf94727b9cf1
olibn <olibn333@gmail.com>
2019-01-12 15:06:57 +0630

    refactored redditScrape and results object creation
    redditScrape now uses promises
    
    doubleScrape gets the articleUrls from redditscrape ready for genericscrape

--- backend/redditScrape.js
+++ backend/redditScrape.js
@@ -1,14 +1,6 @@
 const cheerio = require('cheerio');
-const request = require('request')
-const uuid = require('uuid/v1');
-const scrapeResultHandler = require('./scrapeResultHandler')
 const parseUrl = require('./parseUrl')
 
-scrapeInit()
-
-let globalErrorLog = {
-  errorCount: 0
-}
 
 //Checks if query returns undefined
 function checkUndefined(query) {
@@ -20,107 +12,68 @@ function checkUndefined(query) {
   }
 }
 
-function scrapeInit() {
-  const url = 'https://www.reddit.com/r/futurology'
-  getRedditArticlesFromSubreddit(url)
-  console.log('Scraping ' + url + '...')
-}
+// Creates an ArticlesArray and ErrorLog
+async function getRedditArticlesFromSubreddit(url) {
 
-function createScrapeResultsObject(url, errorLog, articlesArray, scrapeId) {
+  //Load cheerio with HTML
+  const html = await parseUrl.getHTML(url)
+  const $ = cheerio.load(html)
 
-  const scrapeTimestamp = new Date()
-  const domainName = parseUrl.extractRootDomain(url)
-  const hostName = parseUrl.extractHostname(url)
-  const siteName = domainName.split('.')[0]
-
-  //reddit specific:
-  const subReddit = url.split('/r/')[1]
-  const rootSite = url.split('/r/')[0]
-
-  return { scrapeId, url, siteName, rootSite, subReddit, scrapeTimestamp, errorLog, 'articleCount' : articlesArray.length, articlesArray }
-}
-
-function createArticleObject(url, errorLog, articleDetails) {
-
-  const scrapeTimestamp = new Date()
-  const domainName = parseUrl.extractRootDomain(url)
-  const hostName = parseUrl.extractHostname(url)
-  const siteName = domainName.split('.')[0]
-
-  //reddit specific:
-  const subReddit = url.split('/r/')[1]
-  const rootSite = url.split('/r/')[0]
-
-  return Object.assign(articleDetails, { 'sourceUrl': url, errorLog})
-}
-
-function getRedditArticlesFromSubreddit(url) {
-
-  //Error log as object
-  let errorLog = {
+  //Shell objects
+  const errorLog = {
     errorCount: 0
   }
 
-  //let scrapeResultsObject = createScrapeResultsObject(url, errorLog, [])
   let articlesArray = []
-  const scrapeId = uuid()
 
-  //Connect
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+  //Scrape details with cheerio
+  const articles = $('article')
 
-      //Scrape details with cheerio
-      const $ = cheerio.load(body)
-      const articles = $('article')
+  articles.each(function (i, element) {
 
-      articles.each(function (i, element) {
+    let titleEl, titleText, commentsUrl, picUrl, articleUrl, commentsCount, votesCount
 
-        let titleEl, titleText, commentsUrl, picUrl, articleUrl
+    //Title
+    titleEl = $('a h2', element)
+    titleText = checkUndefined(titleEl.text())
 
-        //Title
-        titleEl = $('a h2', element)
-        titleText = checkUndefined(titleEl.text())
-
-        //Reddit URL
-        commentsUrl = checkUndefined(titleEl.parent().attr('href'))
-        if (commentsUrl !== "Not Found") {
-          commentsUrl = url.split('/r/')[0] + commentsUrl
-        }
-        //commentsUrl = checkUndefined(titleEl.parent().attr('href'))
-
-        //Image URL
-        try {
-          const picEl = $('a div[role=img]', element)
-          picUrl = picEl.css('background-image').slice(4, -1)
-        }
-        catch (e) {
-          picUrl = "Not Found"
-          errorLog.errorCount += 1
-        }
-
-        //Article URL
-        articleUrl = checkUndefined($('div a', element).eq(4).attr('href'))
-
-        //Article Id
-        articleId = scrapeId + "-" + i
-
-        //Process
-        const articleDetails = { articleId, titleText, commentsUrl, picUrl, articleUrl }
-
-        globalErrorLog.errorCount += errorLog.errorCount
-
-        // scrapeResultsObject.articlesArray.push(articleDetails)
-        // scrapeResultsObject.errorLog = errorLog
-
-        const articleObject = createArticleObject(url, errorLog, articleDetails)
-
-        articlesArray.push(articleObject)
-        
-      })
-
-      //callback
-      const scrapeResultsObject = createScrapeResultsObject(url, globalErrorLog, articlesArray, scrapeId)
-      scrapeResultHandler(scrapeResultsObject, globalErrorLog)
+    //Reddit URL
+    commentsUrl = checkUndefined(titleEl.parent().attr('href'))
+    if (commentsUrl !== "Not Found") {
+      commentsUrl = url.split('/r/')[0] + commentsUrl
     }
+    //commentsUrl = checkUndefined(titleEl.parent().attr('href'))
+
+    //Image URL
+    try {
+      const picEl = $('a div[role=img]', element)
+      picUrl = picEl.css('background-image').slice(4, -1)
+    }
+    catch (e) {
+      picUrl = "Not Found"
+      errorLog.errorCount += 1
+    }
+
+    //Article URL
+    articleUrl = checkUndefined($('div a', element).eq(4).attr('href'))
+
+    //Article Index
+    articleIndex = i
+
+    //Comments Number
+    commentsCount = $(element).next().find('div > a > span').text().split('comment')[0]
+
+    //Votes
+    votesCount = $(element).parent().siblings().eq(0).text()
+
+    //Process
+    const articleDetails = { articleIndex, titleText, commentsUrl, picUrl, articleUrl, votesCount, commentsCount }
+    articlesArray.push(articleDetails)
+
   })
+  return {articlesArray, errorLog}
 }
+
+module.exports = {getRedditArticlesFromSubreddit}
+
+
