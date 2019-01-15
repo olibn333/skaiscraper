c7f7c8df3456cce60a324cf85465a9c685d99767
joss-c <joss.classey@gmail.com>
2019-01-15 19:57:36 +0400

    refactor(getLogo): await getLogo promise, then define

--- backend/genericScrape.js
+++ backend/genericScrape.js
@@ -20,21 +20,23 @@ function genericScrape(url) {
     const articleTitle = errorLog.checkUndefined($('h1').text())
 
     //Get site logo
-    const getLogo = new Promise((resolve) => {
+    const getLogo = await new Promise((resolve) => {
       scrapeTools.websiteLogo('http://' + parseUrl.extractHostname(url), (error, images) => {
         if (error) console.log(error)
         resolve(images)
       })
     })
 
-    const siteLogo = await getLogo.then((images) => {
-      console.log("SITE LOGO IS.....", images.icon)
-      if (images.openGraph) {
-        return images.openGraph
-      } else {
-        return images.icon
-      }
-    }).catch(error => console.log("IT FUCKED UP", error))
+    const siteLogo = (getLogo.openGraph) ? getLogo.openGraph : getLogo.icon
+
+    // const siteLogo = await getLogo.then((images) => {
+    //   console.log("SITE LOGO IS.....", images.icon)
+    //   if (images.openGraph) {
+    //     return images.openGraph
+    //   } else {
+    //     return images.icon
+    //   }
+    // }).catch(error => console.log("IT FUCKED UP", error))
 
     // const getLogo = async (url) => {
     //   const logo = await scrapeTools.websiteLogo(url, (error, images) => {
