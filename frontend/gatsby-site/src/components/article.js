93b7ffe965f811a4f3d242e7b168ed7c50f06f08
olibn <olibn333@gmail.com>
2019-01-16 20:22:58 +0630

    keywords and img

--- frontend/gatsby-site/src/components/article.js
+++ frontend/gatsby-site/src/components/article.js
@@ -1,38 +1,52 @@
 import React from 'react'
-import { Typography } from '@material-ui/core/';
-import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from '@material-ui/core';
+import {
+  Typography,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Badge
+} from '@material-ui/core';
 import { ExpandMore } from '@material-ui/icons/';
 import { withStyles } from '@material-ui/core/styles';
+import { blueGrey, red } from '@material-ui/core/colors/';
 
 const styles = theme => ({
   root: {
     width: '100%',
+    backgroundColor: blueGrey[100]
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    flexBasis: '33.33%',
     flexShrink: 0,
+    color: red[900],
+    fontWeight: 'bold',
+    verticalAlign: 'middle',
   },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
+  img: {
+    clipPath: 'circle(20%)',
+    marginLeft: '-5%',
+    marginBottom: '0',
   },
+  margin: {
+    margin: theme.spacing.unit * 2,
+  }
 });
 
-const Article = ({data}) => {
+const Article = ({ data, classes }) => {
 
   return (
-    <ExpansionPanel >
-      <ExpansionPanelSummary expandIcon={<ExpandMore/>}>
-        <Typography>
+    <ExpansionPanel className={classes.root} elevation={10}>
+      <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+          <img className={classes.img} alt="complex" src={data.picUrl} />
+        <Typography className={classes.heading}>
           {data.keywords.join(' ')}
         </Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-            <Typography>
-              {data.titleText}
-            </Typography>
-          </ExpansionPanelDetails>
+        <Typography>
+          {data.titleText}
+        </Typography>
+      </ExpansionPanelDetails>
     </ExpansionPanel>
   )
 }
