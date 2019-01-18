3606d2f973a2a2b8f69545ef7436da364ac39781
olibn <olibn333@gmail.com>
2019-01-18 17:59:56 +0630

    sort from graphql not js

--- frontend/gatsby-site/src/pages/getMongoData1.js
+++ frontend/gatsby-site/src/pages/getMongoData1.js
@@ -18,16 +18,16 @@ const articles = ({ data }) => {
   const allArticles = data.allArticles.edges
   const uniqueData = removeDuplicates(allArticles, 'articleUrl')
 
-  const sortedData = uniqueData.sort(
-    function (a, b) {
-      if (a.node.votesCount > b.node.votesCount) { return -1 }
-      if (a.node.votesCount < b.node.votesCount) { return 1 }
-      return 0
-    })
+  // const sortedData = uniqueData.sort(
+  //   function (a, b) {
+  //     if (a.node.votesCount > b.node.votesCount) { return -1 }
+  //     if (a.node.votesCount < b.node.votesCount) { return 1 }
+  //     return 0
+  //   })
   return (
     <Layout>
       <PrimarySearchAppBar simple color={'red'}/>
-      {sortedData.map(({ node }, index) => (
+      {uniqueData.map(({ node }, index) => (
         <Article
           key={index}
           data={node}
@@ -39,7 +39,17 @@ const articles = ({ data }) => {
 
 export const query = graphql`
 {
-  allArticles: allMongodbTestingArticles(limit:20) {
+  allArticles: allMongodbTestingArticles(
+    filter: {
+      votesCount : {
+        gt : 1
+      }
+    }
+    sort : {
+      fields : [votesCount]
+      order : DESC
+    }
+  ) {
     edges {
       node {
         titleText
