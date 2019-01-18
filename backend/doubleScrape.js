afa04b6d169c9bd9802ce62cfc9fd6c33b9246dc
olibn <olibn333@gmail.com>
2019-01-18 10:59:59 +0630

    debug console log remove

--- backend/doubleScrape.js
+++ backend/doubleScrape.js
@@ -5,7 +5,7 @@ const mongoStore = require('./mongoStore')
 
 async function scrapeInit() {
   //Create Scrape Object Shell by parsing initial source url
-  const url = 'https://www.reddit.com/r/futurology'
+  const url = 'https://www.reddit.com/r/artificial'
   const scrapeObjShell = scrapeTools.createScrapeResultsObject(url)
   console.log("Scraping " + url + "...")
 
@@ -23,7 +23,7 @@ async function scrapeInit() {
   const articleUrls = scrapeObj.articlesArray.map((article) => article.articleUrl)
   const articleDetails = await genericScrape.scrapeMultipleSites(articleUrls).catch(error => console.log(error))
 
-  console.log(articleDetails)
+  //console.log(articleDetails)
 
   //Slot details into scrape object
   articleDetails.map((article,i) => {
