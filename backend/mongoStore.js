58f608f4cc8f56f5778b680171e07c03862cc7e7
olibn <olibn333@gmail.com>
2019-01-24 20:06:45 +0630

    async mongo, profiling try

--- backend/mongoStore.js
+++ backend/mongoStore.js
@@ -1,6 +1,52 @@
 const readline = require('readline');
 const MongoClient = require('mongodb').MongoClient;
 
+process.on('unhandledRejection', (reason, promise) => {
+  console.warn('Unhandled promise rejection:', promise, 'reason:', reason.stack || reason);
+});
+
+async function constructMongoUrl() {
+  try {
+    const creds = require('../skai-config')
+    uname = creds.username
+    pword = creds.password
+  }
+  catch (e) {
+    console.log("No local config file found. Please enter username and password:")
+    const creds = await promptUserNameandPassword()
+    uname = creds.username
+    pword = creds.password
+  }
+  const url = "mongodb+srv://" + uname + ":" + pword + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
+  //const url = "mongodb://"+uname+":"+pword+"@cluster0-shard-00-00-ywxua.mongodb.net:27017,cluster0-shard-00-01-ywxua.mongodb.net:27017,cluster0-shard-00-02-ywxua.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+  return url
+}
+
+
+async function setProfile(number) {
+    const url = await constructMongoUrl()
+    return MongoClient.connect(url, { useNewUrlParser: true, forceServerObjectId: true })
+      .then(client => {
+        client.db('test').setProfilingLevel(number)
+        .then(client.close())
+        .catch(e =>console.log(e))
+      })
+    .catch(e => console.log(e))  //skaiScraper-referenced
+  }
+
+  async function readProfiler() {
+    const url = await constructMongoUrl()
+    const level = MongoClient.connect(url, { useNewUrlParser: true, forceServerObjectId: true })
+      .then(client => {
+        console.log(client.db('test').collection('system.profile').stats().catch(e => console.log(e)))
+        .then(db.close())
+        .catch(e =>console.log(e))
+      })
+    .catch(e => console.log(e))  //skaiScraper-referenced
+  } 
+
+  setProfile(2)
+
 
 function sendToDB(file) {
   try {
@@ -11,7 +57,7 @@ function sendToDB(file) {
   }
   catch (e) {
     console.log("No local config file found. Please enter username and password:")
-    promptUserNameandPassword(file, handleResult)
+    promptUserNameandPassword(handleResult)
   }
 }
 
@@ -19,19 +65,22 @@ function handleResult(uname, pword, file) {
   sendToMongoDB(uname, pword, file)
 }
 
-function promptUserNameandPassword(file, callback) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+function promptUserNameandPassword() {
+  const credsPromise = new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
 
-  rl.question('MongoDB Username: ', (uname) => {
-    rl.question('MongoDB Password: ', (pword) => {
-      callback(uname, pword, file)
-      rl.close();
-    })
+    rl.question('MongoDB Username: ', (uname) => {
+      rl.question('MongoDB Password: ', (pword) => {
+        resolve({ uname, pword })
+        rl.close();
+      })
+    });
+  }).catch((err) => console.log("WTF -- " + err))
 
-  });
+  return credsPromise
 };
 
 let newDocs = 0
@@ -40,25 +89,6 @@ function updateCounts(newDs, updatedDs) {
   newDocs = newDocs + newDs;
   updatedDocs = updatedDocs + updatedDs
 }
-
-function setMongoDBProfile(number) {
-  const url = "mongodb+srv://" + username + ":" + password + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
-  MongoClient.connect(url, function (err, db) {
-    db.setProfilingLevel(number)
-  })
-}
-
-function readProfile(){
-  const url = "mongodb+srv://" + username + ":" + password + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
-  MongoClient.connect(url, function (err, db) {
-    const systemDB = db.db('system')
-    systemDB.collection('profile').findOne({},() => {
-      console.log(res)
-    })
-  })
-
-}
-
 
 function sendToMongoDB(username, password, file) {
   const url = "mongodb+srv://" + username + ":" + password + "@cluster0-ywxua.mongodb.net/test?retryWrites=true";
@@ -101,7 +131,7 @@ function sendToMongoDB(username, password, file) {
         { upsert: true },
         function (err, res) {
           // if (err) throw err
-          if (i == articlesArray.length-1) {
+          if (i == articlesArray.length - 1) {
             updateCounts(res.upsertedCount, res.modifiedCount)
             console.log("Updated " + updatedDocs + ", Inserted " + newDocs + " document(s) to articles collection.")
           }
@@ -112,5 +142,6 @@ function sendToMongoDB(username, password, file) {
     db.close()
   })
 }
+
 
 module.exports = { sendToDB }
