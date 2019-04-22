18b020dbe5cfce118531d12cb0c6ac26ee9ebf1d
olibn <olibn333@gmail.com>
2019-04-22 10:48:12 +0700

    auth

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const scrape = require('./doubleScrape')
 
 
 exports.requestScrape = (req, res) => {
-  if (req.body = 'secretpassword') {
+  if (req.body.includes('secretpassword')) {
     scrape.scrapeInit()
     res.status(200).send('Scraping...')} 
   else {
